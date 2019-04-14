abb4a93b9dd5d3ec04db1bacecc5569ce36cc433
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2019-04-13 23:15:04 -0500

    add initial module post creation

---
+++ Device-RAID-Poller/lib/Device/RAID/Poller.pm
@@ -0,0 +1,109 @@
+package Device::RAID::Poller;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Device::RAID::Poller - The great new Device::RAID::Poller!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Device::RAID::Poller;
+
+    my $foo = Device::RAID::Poller->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Zane C. Bowers-Hadley, C<< <vvelox at vvelox.net> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-device-raid-poller at rt.cpan.org>, or through
+the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=Device-RAID-Poller>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Device::RAID::Poller
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=Device-RAID-Poller>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Device-RAID-Poller>
+
+=item * CPAN Ratings
+
+L<https://cpanratings.perl.org/d/Device-RAID-Poller>
+
+=item * Search CPAN
+
+L<https://metacpan.org/release/Device-RAID-Poller>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is Copyright (c) 2019 by Zane C. Bowers-Hadley.
+
+This is free software, licensed under:
+
+  The Artistic License 2.0 (GPL Compatible)
+
+
+=cut
+
+1; # End of Device::RAID::Poller
